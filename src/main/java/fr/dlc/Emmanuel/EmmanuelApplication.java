dd336ffc1ec5c6d45616c27af1307a183007c7dc
Emmanuel6565 <35929864+Emmanuel6565@users.noreply.github.com>
2020-01-17 13:16:28 +0100

    first commit

---
+++ src/main/java/fr/dlc/Emmanuel/EmmanuelApplication.java
@@ -0,0 +1,13 @@
+package fr.dlc.Emmanuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmanuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmmanuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dlc/Emmanuel/EmmanuelApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dlc.Emmanuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmanuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
