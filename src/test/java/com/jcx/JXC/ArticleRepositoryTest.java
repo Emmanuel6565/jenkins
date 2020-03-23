4f38f30d72469866876d01101fecf1b685737039
Emmanuel6565 <emmanuelassamoi65@gmail.com>
2020-03-23 13:00:46 +0100

    changes

--- src/test/java/com/jcx/JXC/ArticleRepositoryTest.java
+++ src/test/java/com/jcx/JXC/ArticleRepositoryTest.java
@@ -37,7 +37,7 @@ public class ArticleRepositoryTest {
         assertEquals(articleRepository.findAll().size(),2);
 
         //unit test 
-        
+        //unit test
         article3 = new Article("110e8400-e29b-11d4-a716-446655440002", "title", "tag", "content");
         articles.add(article3);
         when(articleRepository.findAll()).thenReturn(articles);
