2e68c940ac919f43ece608cf683ffaddd118f909
Emmanuel6565 <emmanuelassamoi65@gmail.com>
2020-05-11 11:47:56 +0200

    feature 001

--- src/main/java/com/jcx/JXC/Controller/ArticleController.java
+++ src/main/java/com/jcx/JXC/Controller/ArticleController.java
@@ -29,7 +29,7 @@ public class ArticleController {
     
     @GetMapping(value = "/feature")
         public String test() {
-            return "feature 001";
+            return "feature 001, j ai rajouté un feature";
         }
 
     @GetMapping(value = "/article/{id}", produces = "application/json")
